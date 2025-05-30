f1d64d121218e1fcd6c6d696108f2d56ea4f875a
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-05-30 11:44:33 +0300

    Labels

---
+++ src/test/java/guru/qa/allure/LabelsTest.java
@@ -0,0 +1,37 @@
+package guru.qa.allure;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LabelsTest {
+
+    @BeforeEach
+    void setUp() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Test
+    public void testAllureLabels1(){
+        Allure.label("owner", "dubinin.s");
+        Allure.feature("Issue");
+        Allure.story("Создание Issue для авторизованного пользователя");
+        Allure.label("severity", SeverityLevel.BLOCKER.value());
+        Allure.link("GitHub", "https://github.com/sergey-dubinin-work/");
+    }
+
+    @Test
+    @Owner("dubinin.s")
+    @Epic("SomeEpic")
+    @Feature("Issue")
+    @Story("Создание Issue")
+    @DisplayName("Создание Issue для авторизованного пользователя")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "GitHub", url = "https://github.com/sergey-dubinin-work/")
+    public void testAllureLabels2(){
+
+    }
+
+}
